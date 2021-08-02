3413b51b8d95016d67783c7d7bec35452505c31b
VictorFarfan98 <vafm98@hotmail.com>
2021-08-02 04:13:14 -0600

    added firebase authentication

--- StoreClient/src/environments/environment.ts
+++ StoreClient/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAHCrYsHLu0b7w-LXJRhc7ZJk6G5EXtpFA",
+    authDomain: "store-74e33.firebaseapp.com",
+    projectId: "store-74e33",
+    storageBucket: "store-74e33.appspot.com",
+    messagingSenderId: "445424693554",
+    appId: "1:445424693554:web:15714feb403e5a7db9e72b",
+    measurementId: "G-RY69HC0NDM"
+  }
 };
 
 /*
