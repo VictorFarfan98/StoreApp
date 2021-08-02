f8620fec75c6fa6294180a66e1473f9ae0ea26db
VictorFarfan98 <vafm98@hotmail.com>
2021-08-01 21:46:41 -0600

    added Product repository

---
+++ StoreAPI/StoreApi/Repositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using StoreApi.Models;
+
+namespace StoreApi.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProduct(int productId);
+        Task<IEnumerable<Product>> GetAllProducts();
+        Task AddProduct(Product product);
+        Task DeleteProduct(int productId);
+        Task EditProduct(Product product);
+    }
+}
---
+++ StoreAPI/StoreApi/Repositories/ProductRepository.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StoreApi.Data;
+using StoreApi.Models;
+
+namespace StoreApi.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDataContext _context;
+        public ProductRepository(IDataContext context)
+        {
+            _context = context;
+        }
+        public async Task AddProduct(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteProduct(int productId)
+        {
+            var productToDelete = await _context.Products.FindAsync(productId);
+            if (productToDelete == null)
+                throw new NullReferenceException();
+            
+            _context.Products.Remove(productToDelete);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task EditProduct(Product product)
+        {
+            var productToEdit = await _context.Products.FindAsync(product.ProductId);
+            if (productToEdit == null) 
+                throw new NullReferenceException();
+            
+            productToEdit.Name = product.Name;
+            productToEdit.Price = product.Price;
+            productToEdit.Stock = product.Stock;
+            productToEdit.UpdatedAt = product.UpdatedAt;
+
+            await _context.SaveChangesAsync();
+        }   
+
+        public async Task<IEnumerable<Product>> GetAllProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product> GetProduct(int productId)
+        {
+            return await _context.Products.FindAsync(productId);
+        }
+    }
+}
